8903a0626f5486a0fad1e9bee4d040cafaf1f710
AlexandrHoroshko <AlexandrHoroshko11@gmail.com>
2019-01-23 18:40:16 +0200

    Update

--- src/test/java/Driver/AutoTestGitHub.java
+++ src/test/java/Driver/AutoTestGitHub.java
@@ -1,5 +1,6 @@
 package Driver;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,6 +8,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AutoTestGitHub {
+
+    @Before
 
     @Test
     public void testGitLogin() throws InterruptedException {
@@ -19,14 +22,43 @@ public class AutoTestGitHub {
         driver.get("https://github.com/login");
 
         WebElement Password = driver.findElement(By.id("password"));
-        WebElement Login = driver.findElement(By.cssSelector("#login_field"));
+        WebElement Login = driver.findElement(By.id("login_field"));
 
 
         Login.sendKeys("AlexandrHoroshko");
-        Password.sendKeys("asdlkfj");
-
-
-        Thread.sleep(5000);
+        Password.sendKeys("wfsifJipGG1995");
+        driver.findElement(By.cssSelector("#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block"))
+                .click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("body > div.application-main > div > div.col-12.col-md-8.col-lg-6.p-responsive.mt-3.border-bottom.d-flex.flex-auto > div > div.flex-auto > div.border.rounded-1.shelf.intro-shelf.js-notice > div > div > a.btn.shelf-cta.ml-3"))
+                .click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#repository_name"))
+                .sendKeys("Test");
+        driver.findElement(By.cssSelector("#repository_public_false"))
+                .click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#new_repository > div.js-with-permission-fields > button"))
+                .click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#js-repo-pjax-container > div.pagehead.repohead.instapaper_ignore.readability-menu.experiment-repo-nav > nav > a:nth-child(6)"))
+                .click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#options_bucket > div.Box.Box--danger > ul > li:nth-child(4) > details > summary"))
+                .click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#options_bucket > div.Box.Box--danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > form > p > input"))
+                .sendKeys("Test");
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#options_bucket > div.Box.Box--danger > ul > li:nth-child(4) > details > details-dialog > div.Box-body.overflow-auto > form > button"))
+                .click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#user-links > li:nth-child(3) > details > summary > span"))
+                .click();
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#user-links > li:nth-child(3) > details > details-menu > form > button"))
+                .click();
+        Thread.sleep(3000);
 
         driver.close();
         driver.quit();
